f1acd960b17cbbae0de945bdb9f832a89e8702e7
Jonas <147929152+jonasbraganca@users.noreply.github.com>
2024-07-13 22:29:07 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-    description = "ID da instancia EC2"
-    value = aws_instance.example.id
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
 }
 
 output "public_ip" {
-    description = "IP publico da instancia EC2"
-    value = aws.instance.example.public_ip 
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.example.public_ip
 }
