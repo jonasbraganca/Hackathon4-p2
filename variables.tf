bb448bbd8c694cf4f4e53f4e61d69a82bb2dedce
Jonas <147929152+jonasbraganca@users.noreply.github.com>
2024-07-13 22:38:38 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "regiao"
   type        = string
   default     = "us-east-1"
 }
 
 variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
+  description = "AMI Id"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The type of instance to deploy"
+  description = "instancia"
   type        = string
   default     = "t2.micro"
 }
